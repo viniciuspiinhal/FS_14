f41a8a6d5657ee4d66cb4bfd63e28e06e474af73
Vinícius Pinhal Alves Souza <viniciuspiinhal@gmail.com>
2022-08-03 20:00:14 -0300

    arquivo de teoria for-while-foreach adicionado

---
+++ Logica/EX3/teoriaForWhileForEach.js
@@ -0,0 +1,33 @@
+// // // let index :  Declaracao da variavel contadora 
+// // // index < array.length :  verificacao da quantidade, quantas vezes ira repetir 
+// // // index++ : incremento 
+// // // lenght : comprimento
+
+// // for (let index = 0; index < 10; index++) {
+ 
+// // }
+
+// // array.forEach(element => {
+    
+// // });
+
+// // while (condition) {
+    
+// // } // o codigo NAO executa ANTES de verificar
+
+// // do {
+    
+// // } while (condition) // o codigo executa SIM ANTES de verificar
+
+// let quantidade = 5
+
+// for (let contador = 0; contador < quantidade; contador++) {
+//     console.log(`o contador é ${contador +1}`)   
+// }
+
+// // while (contador < quantidade) { // while funcionando igual for 
+// //     console.log("Opa, blz")
+// //     contador++
+// // }
+
+// escreve par e o numero correspondente
