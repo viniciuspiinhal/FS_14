b835c3a0cb630dfbb33ec4981c51fe65191779f3
Vinícius Pinhal Alves Souza <viniciuspiinhal@gmail.com>
2022-08-03 19:59:09 -0300

    Exercicio pronto

--- Logica/EX3/contagemalunos.js
+++ Logica/EX3/contagemalunos.js
@@ -1,37 +1,3 @@
-// // // let index :  Declaracao da variavel contadora 
-// // // index < array.length :  verificacao da quantidade, quantas vezes ira repetir 
-// // // index++ : incremento 
-// // // lenght : comprimento
-
-// // for (let index = 0; index < 10; index++) {
- 
-// // }
-
-// // array.forEach(element => {
-    
-// // });
-
-// // while (condition) {
-    
-// // } // o codigo NAO executa ANTES de verificar
-
-// // do {
-    
-// // } while (condition) // o codigo executa SIM ANTES de verificar
-
-// let quantidade = 5
-
-// for (let contador = 0; contador < quantidade; contador++) {
-//     console.log(`o contador é ${contador +1}`)   
-// }
-
-// // while (contador < quantidade) { // while funcionando igual for 
-// //     console.log("Opa, blz")
-// //     contador++
-// // }
-
-// escreve par e o numero correspondente
-
 let numeroDeAlunos =["a","b","c",'d',"e"]  
 for (let contador = 0; contador < numeroDeAlunos.length; contador++) { // metodo 1
  
