fc03790bb45d12fcb233b85164386e850dc19f9d
Vinícius Pinhal Alves Souza <viniciuspiinhal@gmail.com>
2022-10-09 13:55:21 -0300

    Login implementado com JavaScript

--- UC_FRONT/SA2/EC-1/EPlayer/script/script.js
+++ UC_FRONT/SA2/EC-1/EPlayer/script/script.js
@@ -10,3 +10,17 @@
     function mostrarMenuJQuery(){
         $("#menu").toggle()      
     }
+
+    function logar(){
+
+        var login = document.getElementById("login").value;
+        var senha = document.getElementById("senha").value;
+
+        if(login == "admin" && senha == "admin"){
+            alert('Sucesso');
+            location.href = "home.html";
+        }else{
+            alert('Usuario ou senha incorretos');
+        }
+
+    }
