f887b54d018079bfa358ec4cd89008ef11668b22
Vinícius Pinhal Alves <vinicius.pinhal@mapaudi.local>
2023-01-04 16:13:17 -0300

    EC_3 finalizado

---
+++ UC_BACKEND/EC_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using CadastroPessoaFST14.Classes;
+
+PessoaFisica novaPF = new PessoaFisica();
+
+novaPF.nome = "Kalango";
+novaPF.cpf = "11111111111";
+novaPF.rendimento = 12500.5f;
+
+Console.WriteLine (novaPF.nome);    
+
+Console.WriteLine("Nome: "+ novaPF.nome +" - CPF:" + novaPF.cpf);
+Console.WriteLine($"Nome: {novaPF.nome} - CPF: {novaPF.cpf}");
+
+float impostoPagoOk = novaPF.CalcularImposto (novaPF.rendimento);
+
+Console.WriteLine(impostoPagoOk.ToString("C"));
