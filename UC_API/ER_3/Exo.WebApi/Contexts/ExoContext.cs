df418dab4bcedbf8f6e97fe79cf4b03972c2f1c2
Vinicius Trabalho <viniciuspiinhal@gmail.com>
2023-04-26 15:19:33 -0300

    Atividade ER_03 feita

---
+++ UC_API/ER_3/Exo.WebApi/Contexts/ExoContext.cs
@@ -0,0 +1,30 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+
+    public class ExoContext : DbContext
+    {
+
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "User ID=sa;Password=inibirprasubirnogit;"
+                + "Database=ExoApi;Trusted_Connection=True");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
